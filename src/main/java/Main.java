5cb3d732d16b2504eab2b02649dfcfb279f9ece5
Juan Martin Restrepo <tinto922@gmail.com>
2022-06-27 12:48:10 -0400

    inheritance lab done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        System.out.println("======= Running Home Examples ===============");
+        Home baseHome = new Home(); 
+        baseHome.getMadeOf(); 
+        baseHome.getUseFor(); 
+        baseHome.investingInHomes();
+
+        System.out.println("======= Running SingleFamilyHome Examples ===============");
+        SingleFamilyHomes singleFamilyHome = new SingleFamilyHomes(); 
+        singleFamilyHome.getMadeOf(); 
+        singleFamilyHome.getUseFor(); 
+        singleFamilyHome.getNumberOfUnits(); 
+        singleFamilyHome.investingInHomes();
+        singleFamilyHome.valueOfSingleFamilyHome();
+
+        System.out.println("======= Running MultiFamilyHome Examples ===============");
+        MultiFamilyHomes multiFamilyHome = new MultiFamilyHomes(4); 
+        multiFamilyHome.getMadeOf(); 
+        multiFamilyHome.getUseFor(); 
+        multiFamilyHome.getNumberOfUnits(); 
+        multiFamilyHome.investingInHomes();
+        multiFamilyHome.valueOfMultiFamilyHome(); 
     }
 }
